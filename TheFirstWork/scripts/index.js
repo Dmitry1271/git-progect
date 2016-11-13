986e84bfcc47000e0d20d374f460493723b5c4cb
Dmitry <Dmitrij1271@gmail.com>
2016-11-14 01:54:08 +0300

    Added promise

--- TheFirstWork/scripts/index.js
+++ TheFirstWork/scripts/index.js
@@ -9,13 +9,74 @@ function callbackFunc(result) {
 } */
 "use strict";
 
+var info = 'http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=6bfde9f75bb686cfaf1acc61a7abd5cd&artist=Cher&album=Believe&format=json';
 function getJson(url)
+{
+	return new Promise(function(resolve,reject){
+		var data;
+		var template = Handlebars.compile( $('#template').html() );
+		var xhr =new XMLHttpRequest();
+		xhr.open('GET', url ,true);
+		//xhr.responseType = 'json';
+		xhr.onload = function(){
+			if(xhr.status == 200) resolve(xhr.response);
+			if(this.status == 0)
+			{
+				var error0 = new Error(this.statusText);
+				error0.code = this.status;
+				reject(error0);
+			}
+			if(this.status == 403)
+			{
+				var error403 = new Error(this.statusText);
+				error403.code = this.status;
+				reject(error403);
+			}
+			if(this.status == 404)
+			{
+				var error404 = new Error(this.statusText);
+				error404.code = this.status;
+				reject(error404);
+			}
+			if(this.status == 500)
+			{
+				var error500 = new Error(this.statusText);
+				error500.code = this.status;
+				reject(error500);
+			}
+		};
+		xhr.send(); 
+	});
+};
+
+getJson('http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=6bfde9f75bb686cfaf1acc61a7abd5cd&artist=Cher&album=Believe&format=json')
+	.then(
+		response =>{
+			console.log(response);
+			error0 =>{
+				alert('Connection lost. Please check your Internet connection!')
+			} 
+			error403 =>{
+				alert('Forbidden')
+			} 
+			error404 =>{
+				alert('Not found')
+			} 
+			error500 =>{
+				alert('Internal server error')
+			} 
+		});
+
+
+
+/*function getJson(url)
 {
 	var data;
 	var template = Handlebars.compile( $('#template').html() );
 	var xhr =new XMLHttpRequest();
 	xhr.open('GET', url ,true);
 	xhr.send(); 
+
 
 	xhr.onreadystatechange = function(){
 		if(xhr.readyState != 4) 
@@ -36,7 +97,7 @@ function getJson(url)
 
 	button.innerHTML = "Загрузка...";
 	button.disabled = true;
-}
+}*/
 
 
 
