0e22523d2283684f4eee5b05bdc431f2db4440b1
Dmitry <Dmitrij1271@gmail.com>
2016-11-21 16:16:41 +0300

    The last version of progect

--- TheFirstWork/scripts/index.js
+++ TheFirstWork/scripts/index.js
@@ -1,12 +1,3 @@
-/*var script = document.createElement('SCRIPT'); 
-
-script.src = "https://api.vk.com/method/getProfiles?uid=66748&access_token=533bacf...&callback=callbackFunc"; 
-
-document.getElementsByTagName("head")[0].appendChild(script); 
-
-function callbackFunc(result) { 
-  alert(result); 
-} */
 "use strict";
 
 var info = 'http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=6bfde9f75bb686cfaf1acc61a7abd5cd&artist=Cher&album=Believe&format=json';
@@ -18,97 +9,40 @@ function getJson(url)
 		xhr.responseType = 'json';
 		xhr.onload = function(){
 			if(this.status == 200) 
-				{
-					resolve(this.response);
-				}
+			{
+				resolve(this.response);
+			}
+			if(this.status != 200)
+			{
+				var error = new Error(this.statusText);
+				error.code = this.status;
+				reject(error);
+			}
+		};
+		xhr.onerror = function() {
 			if(this.status == 0)
-			{
-				var error0 = new Error(this.statusText);
-				error0.code = this.status;
-				reject(error0);
-			}
+				reject(new Error("Connection lost. Please check your Internet connection!"));
 			if(this.status == 403)
-			{
-				var error403 = new Error(this.statusText);
-				error403.code = this.status;
-				reject(error403);
-			}
+				reject(new Error("Forbidden"));
 			if(this.status == 404)
-			{
-				var error404 = new Error(this.statusText);
-				error404.code = this.status;
-				reject(error404);
-			}
+				reject(new Error("Not found"));
 			if(this.status == 500)
-			{
-				var error500 = new Error(this.statusText);
-				error500.code = this.status;
-				reject(error500);
-			}
+				reject(new Error("Internal server error"));
 		};
 		xhr.send(); 
 	});
 };
-
-getJson('http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=6bfde9f75bb686cfaf1acc61a7abd5cd&artist=Cher&album=Believe&format=json')
+$(document).ready(function() {
+	getJson('http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=6bfde9f75bb686cfaf1acc61a7abd5cd&artist=Cher&album=Believe&format=json')
 	.then(
 		response =>{
 			console.log(response);
 			var data;
-		  var template = Handlebars.compile( $('#template').html() );
+			var template = Handlebars.compile( $('#template').html() );
 			$('.updates').append( template(response) );
 		},
-			error0 =>{
-				alert('Connection lost. Please check your Internet connection!')
-			} ,
-			error403 =>{
-				alert('Forbidden')
-			} ,
-			error404 =>{
-				alert('Not found')
-			}, 
-			error500 =>{
-				alert('Internal server error')
-			});
-
-
-
-/*function getJson(url)
-{
-	var data;
-	var template = Handlebars.compile( $('#template').html() );
-	var xhr =new XMLHttpRequest();
-	xhr.open('GET', url ,true);
-	xhr.send(); 
-
-
-	xhr.onreadystatechange = function(){
-		if(xhr.readyState != 4) 
-			return;
-
-		button.innerHTML = "Готово!";
-
-		if(xhr.status != 200) alert(xhr.status + ": " + xhr.statusText);
-		else {
-			data = xhr.responseText;
-			console.log(data);
-			$('.updates').append( template(data) );
-		}
-	}
-
-	
-
-
-	button.innerHTML = "Загрузка...";
-	button.disabled = true;
-}*/
-
-
-
-/*
-Here are the details of your new API account.
-Application name	Abaddon1271
-API key	6bfde9f75bb686cfaf1acc61a7abd5cd
-Shared secret	f2440417c4dc962a357d66d6edb5aee1
-Registered to	Abaddon1271
-*/
+		error => {
+			console.log(error);
+		});
+	setInterval(getJson, 10000);
+});
